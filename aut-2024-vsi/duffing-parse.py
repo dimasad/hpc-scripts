52abbc83726cc445bf0db237e690a325ecb1e46d
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2024-01-29 21:59:17 -0500

    Fixed typo.

--- aut-2024-vsi/duffing-parse.py
+++ aut-2024-vsi/duffing-parse.py
@@ -12,35 +12,37 @@ if __name__ == '__main__':
         with open( case.name + ".txt", "w") as outfile:
             entries = case.glob('tf_*')
             for entry in entries:
-                tf = float(entry.name[3:])
-                for rep in entry.glob('*.pickle'):
-                    time = []
-                    qerr = []
-                    qsyserr = []
-                    x0err = []
-                    x1err = []
-                    with open(rep, 'rb') as f:
-                        data = pickle.load(f)
-                        xsim = data['sim']['x'][::4]
-                        if case.name.startswith('sk-'):
-                            nwin = data['args']['nwin']
-                            xsim = xsim[nwin:-nwin]
-                        qsim = data['sim']['q']
-                        qest = data['decopt']['q']
-                        xest = data['vopt']['xbar']
-                        qerr.append(np.abs(qsim - qest).mean())
-                        qsyserr.append(np.abs(qsim - qest)[:-2].mean())
-                        x0err.append(np.abs(xsim[:,0] - qest[:,0]).mean())
-                        x1err.append(np.abs(xsim[:,1] - qest[:,1]).mean())
-                        time.append(data['time_elapsed'])
+                try:
+                    tf = float(entry.name[3:])
+                    for rep in entry.glob('*.pickle'):
+                        time = []
+                        qerr = []
+                        qsyserr = []
+                        x0err = []
+                        x1err = []
+                        with open(rep, 'rb') as f:
+                            data = pickle.load(f)
+                            xsim = data['sim']['x'][::4]
+                            if case.name.startswith('sk-'):
+                                nwin = data['args']['nwin']
+                                xsim = xsim[nwin:-nwin]
+                            qsim = data['sim']['q']
+                            qest = data['decopt']['q']
+                            xest = data['vopt']['xbar']
+                            qerr.append(np.abs(qsim - qest).mean())
+                            qsyserr.append(np.abs(qsim - qest)[:-2].mean())
+                            x0err.append(np.abs(xsim[:,0] - xest[:,0]).mean())
+                            x1err.append(np.abs(xsim[:,1] - xest[:,1]).mean())
+                            time.append(data['time_elapsed'])
 
-                print(
-                    tf,
-                    np.min(time), np.mean(time), np.max(time), 
-                    np.min(qerr), np.mean(qerr), np.max(qerr),
-                    np.min(qsyserr), np.mean(qsyserr), np.max(qsyserr),
-                    np.min(x0err), np.mean(x0err), np.max(x0err),
-                    np.min(x1err), np.mean(x1err), np.max(x1err),
-                    file=outfile
-                )
-
+                    print(
+                        tf,
+                        np.min(time), np.mean(time), np.max(time), 
+                        np.min(qerr), np.mean(qerr), np.max(qerr),
+                        np.min(qsyserr), np.mean(qsyserr), np.max(qsyserr),
+                        np.min(x0err), np.mean(x0err), np.max(x0err),
+                        np.min(x1err), np.mean(x1err), np.max(x1err),
+                        file=outfile
+                    )
+                except Exception as e:
+                    print(e)
