72de460bb9d7424888583e6dfff857b71753aad6
Dimas Abreu Archanjo Dutra <dimas.dutra@mail.wvu.edu>
2024-01-30 13:07:22 -0500

    Improvements in duffing-parse.py

--- aut-2024-vsi/duffing-parse.py
+++ aut-2024-vsi/duffing-parse.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 
+import argparse
 import pathlib
 import pickle
 
 import numpy as np
 
 
-if __name__ == '__main__':
-    cases = filter(pathlib.Path.is_dir, pathlib.Path('.').glob('*'))
+def parse(dir):
+    cases = filter(pathlib.Path.is_dir, pathlib.Path(dir).glob('*'))
+    data = {}
     for case in cases:
+        data[case.name] = []
         with open( case.name + ".txt", "w") as outfile:
             entries = case.glob('tf_*')
             for entry in entries:
                 tf = float(entry.name[3:])
                 time = []
                 qerr = []
-                qsyserr = []
-                x0err = []
-                x1err = []
+                xerr = []
+                no_result = []
                 for rep in entry.glob('*.pickle'):
                     with open(rep, 'rb') as f:
                         try:
@@ -29,22 +31,39 @@ if __name__ == '__main__':
                             qsim = data['sim']['q']
                             qest = data['decopt']['q']
                             xest = data['vopt']['xbar']
-                            qerr.append(np.abs(qsim - qest).mean())
-                            qsyserr.append(np.abs(qsim - qest)[:-2].mean())
-                            x0err.append(np.abs(xsim[:,0] - xest[:,0]).mean())
-                            x1err.append(np.abs(xsim[:,1] - xest[:,1]).mean())
+                            qerr.append(np.abs(qsim - qest))
+                            xerr.append(np.abs(xsim - xest).mean(0))
                             time.append(abs(data['time_elapsed']))
                         except Exception as e:
+                            no_result.append(rep)
                             print(rep, e)
-                try:
-                    print(
-                        tf,
-                        np.min(time), np.mean(time), np.max(time), 
-                        np.min(qerr), np.mean(qerr), np.max(qerr),
-                        np.min(qsyserr), np.mean(qsyserr), np.max(qsyserr),
-                        np.min(x0err), np.mean(x0err), np.max(x0err),
-                        np.min(x1err), np.mean(x1err), np.max(x1err),
-                        file=outfile
-                    )
-                except:
-                    pass
+                data[case.name].append(
+                    tf=tf,
+                    time=time,
+                    qerr=qerr,
+                    xerr=xerr,
+                    no_result=no_result,
+                )
+    return data
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', type=pathlib.Path, default=pathlib.Path('.'))
+    args = parser.parse_args()
+
+    data = parse(args.dir)
+
+    # Write binary data
+    with open(args.dir / 'data.pickle', 'wb') as f:
+        pickle.dump(data, f)
+    
+    # Write text data    
+    for k, v in data.items():
+        with open(args.dir / (k + '.txt'), 'w') as f:
+            for entry in v:
+                f.write(
+                    entry.tf,
+                    entry.time.min(), entry.time.mean(), entry.time.max(),
+                    *entry.qerr.min(0), *entry.qerr.mean(0), *entry.qerr.max(0),
+                    *entry.xerr.min(0), *entry.xerr.mean(0), *entry.xerr.max(0),
+                )
